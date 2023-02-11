6b9366983d6328907734272e05bc8da77f516949
Vladimir <vvbabenko87@yandex.ru>
2023-02-11 20:56:14 +0300

    ad]

--- src/components/app-header/App-header-link.jsx
+++ src/components/app-header/App-header-link.jsx
@@ -1,21 +1,27 @@
 // import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+import styles from './App-header-link.module.css'
 
 const AppHeaderLink = (props) => {
 
     return (
-        <>
-            {props.icon}
-            <p className="text text_type_main-default">
+        <NavLink
+            to={props.to}
+            className={styles.link}>
+           {props.icon}
+            <span className="text text_type_main-default">
                 {props.children}
-            </p>
-        </>
+            </span>
+        </NavLink>
     )
 }
 
 AppHeaderLink.propTypes = {
     icon: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    to: PropTypes.string.isRequired
 }
 export default AppHeaderLink;
--- src/components/app-header/App-header.jsx
+++ src/components/app-header/App-header.jsx
@@ -1,45 +1,30 @@
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React, { useState } from 'react';
-import AppHeaderLink from './App-header-link'
+import React from 'react';
+import AppHeaderLink from './App-header-link';
+
 import styles from './App-header.module.css'
 
 const AppHeader = () => {
-    const [state, setState] = useState({
-        burgerIcon: 'secondary',
-        listIcon: 'secondary',
-        profileIcon: 'secondary',
-    })
-
-    const handleMouseEnter = (e) => {
-        if (e.target.id === 'BurgerIcon') setState({ ...state, burgerIcon: 'primary', profileIcon: 'secondary', listIcon: 'secondary' })
-        else if (e.target.id === 'ProfileIcon') setState({ ...state, profileIcon: 'primary', burgerIcon: 'secondary', listIcon: 'secondary' })
-        else if (e.target.id === 'ListIcon') setState({ ...state, listIcon: 'primary', profileIcon: 'secondary', burgerIcon: 'secondary' })
-    }
-
-    const handleMouseLeave = () => {
-        setState({ burgerIcon: 'secondary', profileIcon: 'secondary', listIcon: 'secondary' })
-    }
-
+    
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <nav className={styles.nav}>
                     <ul className={styles.navUl}>
-                        <li className={styles.link}
-                            id='BurgerIcon'
-                            onMouseEnter={handleMouseEnter}
-                            onMouseLeave={handleMouseLeave}
-                        >
-                            <AppHeaderLink icon={<BurgerIcon type={state.burgerIcon} />}>
+                        <li
+                            className={styles.headeLink}>
+                            <AppHeaderLink
+                                icon={<BurgerIcon  />}
+                                to={'/'}
+                            >
                                 Конструктор
                             </AppHeaderLink>
                         </li>
-                        <li className={styles.link}
-                            id='ListIcon'
-                            onMouseEnter={handleMouseEnter}
-                            onMouseLeave={handleMouseLeave}
+                        <li className={styles.headeLink} >
+                            <AppHeaderLink
+                                icon={<ListIcon />}
+                                to={'/'}
                             >
-                            <AppHeaderLink icon={<ListIcon type={state.listIcon} />}>
                                 Лента заказов
                             </AppHeaderLink>
                         </li>
@@ -48,13 +33,11 @@ const AppHeader = () => {
                 <div className={styles.wrapperLogo}>
                     <Logo />
                 </div>
-                <div
-                    className={`${styles.link} ${styles.linkProfile}`}
-                    id='ProfileIcon'
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    <AppHeaderLink icon={<ProfileIcon type={state.profileIcon} />}>
+                <div className={`${styles.link} ${styles.headeLinkProfile}`}>
+                    <AppHeaderLink
+                        icon={<ProfileIcon  />}
+                        to={'/profile'}
+                    >
                         Личный кабинет
                     </AppHeaderLink>
                 </div>
