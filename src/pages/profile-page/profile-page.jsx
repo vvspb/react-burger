5481139d1fc3bb93727180ab5be302714a609f8d
Vladimir <vvbabenko87@yandex.ru>
2023-02-11 11:33:28 +0300

    added profile-page

---
+++ src/pages/profile-page/profile-page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppHeader from '../../components/app-header/App-header';
+import styles from './profile-page.module.css';
+
+const ProfilePage = () => {
+  return (
+    <div>
+        <AppHeader/>
+        <main className={styles.main}>
+       
+        </main>
+
+    </div>
+  )
+}
+
+export default ProfilePage
