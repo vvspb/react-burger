f498a631a757f54b3e91e4e84be32d55795368a9
Vladimir <vvbabenko87@yandex.ru>
2023-02-11 11:11:02 +0300

    added react-route-dom, added folder pages, refactoring code into App component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,42 +1,18 @@
-import { useState } from 'react';
-import AppHeader from '../app-header/App-header';
-import BurgerIngredients from '../burger-ingredients/Burger-ingredients';
-import BurgerConstructor from '../burger-constructor/Burger-constructor';
-import SumOrderContext from '../../contexts/sumOrderContext';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
-import { useSelector } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from '../../pages/main-page/main-page';
 
 import styles from './App.module.css'
 
 
-
 function App() {
-
-
-
-  const [sumOrder, setSumOrder] = useState(0)
-
-  const { isLoading } = useSelector((state) => state)
-
 
   return (
     <div className={styles.app}>
-      <SumOrderContext.Provider value={{ sumOrder, setSumOrder }}>
-        <AppHeader />
-        <main className={styles.main}>
-          {
-            isLoading ?
-              <p className={styles.load}><span className='text text_type_main-medium'>идет загрузка...</span></p>
-              :
-                <DndProvider backend={HTML5Backend}>
-                  <BurgerIngredients />
-                  <BurgerConstructor />
-                </DndProvider>
-          }
-        </main>
-      </SumOrderContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import AppHeader from '../../components/app-header/App-header';
+import BurgerIngredients from '../../components/burger-ingredients/Burger-ingredients';
+import BurgerConstructor from '../../components/burger-constructor/Burger-constructor';
+import SumOrderContext from '../../contexts/sumOrderContext';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { useSelector } from 'react-redux';
+
+import styles from './main-page.module.css'
+
+const MainPage = () => {
+
+  const [sumOrder, setSumOrder] = useState(0)
+  const { isLoading } = useSelector((state) => state)
+
+  return (
+    <div>
+       <SumOrderContext.Provider value={{ sumOrder, setSumOrder }}>
+        <AppHeader />
+        <main className={styles.main}>
+          {
+            isLoading ?
+              <p className={styles.load}><span className='text text_type_main-medium'>идет загрузка...</span></p>
+              :
+              <DndProvider backend={HTML5Backend}>
+                <BurgerIngredients />
+                <BurgerConstructor />
+              </DndProvider>
+          }
+        </main>
+      </SumOrderContext.Provider>
+    </div>
+  )
+}
+
+export default MainPage
