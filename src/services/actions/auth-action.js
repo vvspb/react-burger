1ea02fe4a92cc582bfdb33828568134a28c0b537
Vladimir <vvbabenko87@yandex.ru>
2023-02-16 18:08:37 +0300

    added action for request getUser

--- src/services/actions/auth-action.js
+++ src/services/actions/auth-action.js
@@ -1,10 +1,14 @@
-import { GET_USERDATA, GET_USERDATA_SUCCESS, GET_USERDATA_FAILURE } from '../actions-types/auth-action-type';
+import { GET_USERDATA, GET_USERDATA_SUCCESS, GET_USERDATA_FAILURE, DELETE_USERDATA, DELETE_USERDATA_SUCCESS, AUTH_CHECKED } from '../actions-types/auth-action-type';
 
 import api from '../../utils/api';
-import { setCookie } from '../../utils/cookie';
+import { getCookie, setCookie } from '../../utils/cookie';
 
 export const authUserData = () => ({
     type: GET_USERDATA,
+})
+
+export const deleteUserData = () => ({
+    type: DELETE_USERDATA,
 })
 
 export const authUserDataSuccess = (userData) => ({
@@ -12,8 +16,16 @@ export const authUserDataSuccess = (userData) => ({
     payload: userData
 })
 
+export const deleteUserDataSuccess = () => ({
+    type: DELETE_USERDATA_SUCCESS
+})
+
 export const authUserDataFailure = () => ({
     type: GET_USERDATA_FAILURE
+})
+
+export const authChecked = () => ({
+    type: AUTH_CHECKED
 })
 
 export const fetchAuthUserData = (email, password, name = 0) => {
@@ -28,5 +40,30 @@ export const fetchAuthUserData = (email, password, name = 0) => {
                 }
             })
             .catch(() => dispatch(authUserDataFailure()))
+    }
+}
+
+export const checkUserAuth = () => (dispatch) => {
+    if (getCookie("accessToken")) {
+        api.getUser()
+        .then(result => {
+            if (result.success) return dispatch(authUserDataSuccess(result.user))
+        })
+        .catch(()=> {dispatch(authChecked());})
+        .finally(() => {
+            dispatch(authChecked());
+        });
+    } else {
+        dispatch(authChecked());
+    }
+};
+
+export const fetchLogoutUserData = () => {
+    return (dispatch) => {
+        dispatch(deleteUserData())
+        api.logoutUser()
+            .then(result => {
+                if (result.success) return dispatch(deleteUserDataSuccess(result.user))
+            })
     }
 }
