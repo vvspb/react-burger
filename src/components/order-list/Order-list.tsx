30e9b7fcf22313fd8cbeb5a14c08d307fa2c57e4
Vladimir <vvbabenko87@yandex.ru>
2023-03-25 18:36:13 +0300

    added revers array of personal orders

--- src/components/order-list/Order-list.tsx
+++ src/components/order-list/Order-list.tsx
@@ -1,25 +1,36 @@
 import React, { FC } from 'react';
 
 import { useSelector } from '../../hooks/hooks';
+import { IOrders } from '../../utils/types';
 import OrderCard from '../order-card/Order-card';
 import styles from './Order-list.module.css'
 
 type TPropsOrderList = {
   flag: boolean;
 }
-const OrderList: FC<TPropsOrderList> = ({flag}: TPropsOrderList) => {
+const OrderList: FC<TPropsOrderList> = ({ flag }: TPropsOrderList) => {
 
   const { orderFeed, orderFeedPersonal } = useSelector(store => store.wsOrderFeed);
 
+  const ordersPersonal = orderFeedPersonal?.orders
+  const ordersPersonalReversed: IOrders[] = []
+
+  if (ordersPersonal) {
+    for (let i = ordersPersonal.length - 1; i >= 0; i--) {
+      ordersPersonalReversed?.push(ordersPersonal[i])
+    }
+  }
+
+  const orders = flag ? ordersPersonalReversed : orderFeed?.orders
 
   return (
-    <ul className={`${styles.wrappContaienrList} ${flag? styles.wrappPersonalOrder: styles.wrappFeedOrder}`}>
+    <ul className={`${styles.wrappContaienrList} ${flag ? styles.wrappPersonalOrder : styles.wrappFeedOrder}`}>
       {
-      (flag? orderFeedPersonal : orderFeed)?.orders?.map(item => (
-        <li key={item._id}>
-            <OrderCard {...item}  flag={flag}/>
-        </li>))!
-        }
+        orders?.map(item => (
+          <li key={item._id}>
+            <OrderCard {...item} flag={flag} />
+          </li>))!
+      }
     </ul>
   )
 }
