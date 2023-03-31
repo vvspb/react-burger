dbac5af2166c5e561569791c32af0692243527ff
Vladimir <vvbabenko87@yandex.ru>
2023-03-31 19:01:04 +0300

    fixed code

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import store from './services/store';
 
 
